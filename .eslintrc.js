3ed3f5797c0cf03b4ead24d6e6b403a97601d01e
YongYuH <larry830326@hotmail.com>
2021-07-18 23:32:55 +0800

    chore: add eslint and git hook

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+/* eslint-env node */
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier', 'react', 'simple-import-sort'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:react/recommended',
+  ],
+  rules: {
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: false,
+      },
+    ],
+  },
+  settings: {
+    react: {
+      version: '17.0.2',
+    },
+  },
+}
